ccb18e6d6682df6f2a034d905c464803cf75f2cc
lidiagdcso <lidia.gallardodc@gmail.com>
2025-07-20 17:03:16 +0200

    Creacion de variable games

---
+++ main.js
@@ -0,0 +1,96 @@
+let games = [
+  {
+    name: 'Versailles',
+    price: "4'95€",
+    seller: 'Zacatrus',
+    image:
+      'https://media.zacatrus.com/catalog/product/cache/f22f70ef8ee260256901b557cf6bf49a/v/e/versailles-juego-de-mesa.jpg'
+  },
+
+  {
+    name: 'Tholos',
+    price: "12'50€",
+    seller: 'Zacatrus',
+    image:
+      'https://media.zacatrus.com/catalog/product/cache/ede91dc607ff58d6ff51aded48a7a72e/t/h/tholos_2.jpg'
+  },
+
+  {
+    name: 'Zenith',
+    price: "33'00€",
+    seller: 'Zacatrus',
+    image:
+      'https://media.zacatrus.com/catalog/product/cache/ede91dc607ff58d6ff51aded48a7a72e/z/e/zenith-render_resultado.jpg'
+  },
+
+  {
+    name: 'Mala Suerte',
+    price: "13'95€",
+    seller: 'Zacatrus',
+    image: 'https://media.zacatrus.com/amasty/amlabel/Envio-Listado.png'
+  },
+
+  {
+    name: 'Cathood: Family and Friends',
+    price: "9'95€",
+    seller: 'Zacatrus',
+    image:
+      'https://media.zacatrus.com/catalog/product/cache/ede91dc607ff58d6ff51aded48a7a72e/c/a/cathood-family-and-friends.png'
+  },
+
+  {
+    name: 'One Piece Nakama: Amigos y Enemigos',
+    price: "22'50€",
+    seller: 'Zacatrus',
+    image:
+      'https://media.zacatrus.com/catalog/product/cache/ede91dc607ff58d6ff51aded48a7a72e/o/n/one-piece-nakama-friends-enemies-en-espanol-juego-de-mesa-de-tcg-factory.jpg'
+  },
+
+  {
+    name: "Jus'Fight",
+    price: "13'49€",
+    seller: 'Zacatrus',
+    image:
+      'https://media.zacatrus.com/catalog/product/cache/ede91dc607ff58d6ff51aded48a7a72e/j/u/jusfight_resultado.jpg'
+  },
+
+  {
+    name: 'Cochinilla Porteerilla',
+    price: "17'99€",
+    seller: 'Zacatrus',
+    image:
+      'https://media.zacatrus.com/catalog/product/cache/ede91dc607ff58d6ff51aded48a7a72e/c/o/cochinillaporterilla_resultado.jpg'
+  },
+
+  {
+    name: 'Fake News',
+    price: "13'49€",
+    seller: 'Zacatrus',
+    image:
+      'https://media.zacatrus.com/catalog/product/cache/ede91dc607ff58d6ff51aded48a7a72e/f/a/fakenews_resultado.jpg'
+  },
+
+  {
+    name: 'Harry Potter: Un año en Hogwarts',
+    price: "31'49€",
+    seller: 'Zacatrus',
+    image:
+      'https://media.zacatrus.com/catalog/product/cache/ede91dc607ff58d6ff51aded48a7a72e/u/n/unanoenhowarts_resultado.jpg'
+  },
+
+  {
+    name: 'MONSTArgh!',
+    price: "6'75€",
+    seller: 'Zacatrus',
+    image:
+      'https://media.zacatrus.com/catalog/product/cache/ede91dc607ff58d6ff51aded48a7a72e/m/o/monstargh_2.jpg'
+  },
+
+  {
+    name: 'Hombre Lobo: El libro de Wyrm',
+    price: "11'64€",
+    seller: 'Zacatrus',
+    image:
+      'https://media.zacatrus.com/catalog/product/cache/ede91dc607ff58d6ff51aded48a7a72e/l/i/libro_wyrm_nwebm_2.jpg'
+  }
+]
